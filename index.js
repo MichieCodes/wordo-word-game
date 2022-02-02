d076d60eed23c4b84a5d2f03d77d6b8f898f75b3
MichieTuts <michietuts@gmail.com>
2022-02-01 23:58:56 -0600

    Test node logic

---
+++ index.js
@@ -0,0 +1,92 @@
+const readline = require('readline');
+
+const sc = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+sc.ask = (query) => new Promise((res) => sc.question(query, res));
+
+const VALIDATION_STATES = {
+    correct: 'g',
+    exists: 'y',
+    incorrect: 'r'
+};
+
+/**
+*   @param {string[]} word
+*   @returns {Object} 
+*/
+function getLetterFrequency(word) {
+    let frequency = {};
+
+    for(let c of word) {
+        frequency[c] ||= 0;
+        frequency[c]++;
+    }
+    
+
+    return frequency;
+}
+
+/**
+*   @param {string[]} word
+*   @param {string} guess
+*   @returns {string[]} 
+*/ 
+function checkAccuracy(word, frequency, guess) {
+    frequency = {...frequency};
+
+    const accuracy = word.map((char, i) => {
+        const guess_char = guess[i];
+
+        if(guess_char === char)
+            return VALIDATION_STATES.correct;
+        else if(frequency[guess_char] && frequency[guess_char]--)
+            return VALIDATION_STATES.exists;
+        else 
+            return VALIDATION_STATES.incorrect;
+
+    });
+
+    return accuracy;
+}
+
+async function playWordle(word) {
+    word = [...word.toUpperCase()];
+    
+    let count = 0;
+
+    while(true) {
+        if(++count > 6) {
+            console.log("YOU LOST");
+            break;
+        }
+
+        let guess = (await sc.ask('Guess a word:\t')).toUpperCase();
+
+        if(guess == 'Q') break;
+        else if(guess.length != 5) {
+            count--;
+            continue;
+        }
+
+        const frequency = getLetterFrequency(word);
+        const accuracy = checkAccuracy(word, frequency, guess);
+ 
+        console.log('Guess : ', [...guess]);
+        console.log('Result: ', accuracy);
+
+        if(guess == word.join('')) {
+            console.log('YOU WON');
+            break;
+        }
+     }
+
+    console.log("WORD WAS:\t", word.join(''));
+    sc.close(); 
+}
+
+// playWordle('canal');
+playWordle('could');
+
