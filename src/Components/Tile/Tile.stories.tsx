21bd5c24688d1ea7ca94680e66a97544419f6871
MichieTuts <michietuts@gmail.com>
2022-02-11 23:55:46 -0600

    Remove types from Tile story

--- src/Components/Tile/Tile.stories.tsx
+++ src/Components/Tile/Tile.stories.tsx
@@ -6,11 +6,13 @@ import Tile from './Tile';
 export default {
   title: 'Wordle/Tile',
   component: Tile,
-} as ComponentMeta<typeof Tile>;
-
-const Template: ComponentStory<typeof Tile> = () => <Tile/>;
-
-export const GreenTile = Template.bind({});
-GreenTile.args = {
-  label: 'Button',
 };
+
+// const Template: ComponentStory<typeof Tile> = () => <Tile/>;
+
+// export const GreenTile = Template.bind({});
+// GreenTile.args = {
+  // label: 'Button',
+// };
+
+export const GreenTile = () => <Tile/>
